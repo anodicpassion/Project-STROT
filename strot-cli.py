e2d43e6a886fff36befb4706606fd1aa198d751c
anodicpassion <pratikpawarkarad@gmail.com>
2025-03-22 03:00:50 +0530

    animation: reduced the animation size

--- strot-cli.py
+++ strot-cli.py
@@ -368,7 +368,7 @@ class STROTCLI:
 
 if __name__ == "__main__":
 
-    with open("splash/owl", "rb") as adf:
+    with open("splash/anime_dump", "rb") as adf:
         animation = pickle.load(adf)
     os.system('clear')
     k = 0
